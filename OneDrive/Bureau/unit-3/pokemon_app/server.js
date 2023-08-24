ff382b3b82bd40fa2d233c6e42d737bf9005a9be
zineb790 <nohad91@hotmail.fr>
2023-08-24 19:16:42 -0400

    connected my database , can see json in the browser

--- OneDrive/Bureau/unit-3/pokemon_app/server.js
+++ OneDrive/Bureau/unit-3/pokemon_app/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const pokemon = require('./models/pokemon.js');
 
 
 app.get('/', (req,
     res) => {
     res.send("<h1>Welcome to the Pokemon App!</h1>")
+});
+
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon);
 });
 
 
