f1037d338a18125e1be673d92ed0dab6704909e2
zineb790 <nohad91@hotmail.fr>
2023-08-24 21:13:14 -0400

    I can view a list of all my pokemon in the browser

--- OneDrive/Bureau/unit-3/pokemon_app/views/Index.jsx
+++ OneDrive/Bureau/unit-3/pokemon_app/views/Index.jsx
@@ -1,15 +1,23 @@
 const React = require('react');
+
 const myStyle = {
 color: '#ffffff',
 backgroundColor: '#000000',
+};
+
+const capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 };
 class Index extends React.Component {
     render() {
         return (
             <div>
-                <div style={myStyle}>
-                    <h1>See All The Pokemon!</h1>
-                </div>
+                <div style={myStyle}><h1>See All The Pokemon!</h1></div>
+                    <ul>
+                    {this.props.pokemon?.map((pok, i) => {
+                   return (
+                   <li key={i}>{capitalizeFirstLetter(pok.name)}</li>)})}
+                    </ul>
               
             </div>
         );
