2fe08eeb17e0a9eae801fc2b276397663ed245bc
zineb790 <nohad91@hotmail.fr>
2023-09-05 21:35:17 -0400

    added sign up form

---
+++ OneDrive/Bureau/unit-3/mern-infrastructure/src/pages/AuthPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignUpForm from '../components/SignUpForm/SignUpForm'
+
+export default function AuthPage() {
+  return (
+    <div>
+        <h1>AuthPage</h1>
+        <SignUpForm />
+    </div>
+  )
+}
---
+++ OneDrive/Bureau/unit-3/mern-infrastructure/src/pages/OrderHistoryPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OrderHistoryPage() {
+  return (
+    <div>
+        <h1>OrderHistoryPage</h1>
+    </div>
+  )
+}
